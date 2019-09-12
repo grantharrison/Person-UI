b06b4bb56fbdfb6e341bab46dbed824a8a10f0c3
grantharrison <gthgg6@mail.missouri.edu>
2019-09-12 16:44:58 -0500

    Initial Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Grant Harrison on 9/12/19.
+//  Copyright © 2019 Grant Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
